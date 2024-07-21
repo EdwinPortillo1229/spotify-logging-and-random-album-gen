355e1793baf7a98eb7ab1147a1cad4dfc4af6862
Edwin Portillo <edwinportillo@edwins-mbp.mynetworksettings.com>
2024-07-21 13:41:25 -0400

    wipperoni

--- app/models/spotify_user.rb
+++ app/models/spotify_user.rb
@@ -2,9 +2,8 @@ class SpotifyUser < ActiveRecord::Base
   SCOPES = %w(user-library-read user-read-email user-read-private user-library-modify)
   SPOTIFY_URL = "https://accounts.spotify.com/authorize?"
   CLIENT_ID = "a2cb6f3e5f7340c3a1ed328590fab4b2"
-  CLIENT_SECRET = "d974eb738ed24ae0bcb6efdb279f782e"
+  CLIENT_SECRET = "3333183bf89e4639a8a8a549dde8a9e3"
   REDIRECT_URI_LINK = "http://127.0.0.1:3000/link_spotify"
-  REDIRECT_URI_LINKED = "http://127.0.0.1:3000/linked_spotify"
 
   def self.generate_state
     SecureRandom.hex(16)
@@ -21,15 +20,28 @@ class SpotifyUser < ActiveRecord::Base
 
   def self.generate_spotify_token_post_url(code)
     client_substring = "#{CLIENT_ID}:#{CLIENT_SECRET}"
-    form = {
+
+    data = {
+      grant_type: 'authorization_code',
       code: code,
-      redirect_uri: REDIRECT_URI_LINKED,
-      grant_type: 'authorization_code'
+      redirect_uri: REDIRECT_URI_LINK
     }
+
+    encoded_data = data.map { |k,v| "#{k}=#{CGI.escape(v.to_s)}" }.join('&')
 
     headers = {
       'content-type'=> 'application/x-www-form-urlencoded',
-      'Authorization' => "basic #{Base64.encode(CLIENT_ID + CLIENT_SECRET)}"
+      'Authorization' => "basic #{Base64.strict_encode64(client_substring)}"
     }
+   
+    response = HTTParty.post(
+      "https://accounts.spotify.com/api/token",
+      headers: headers,
+      body: encoded_data
+    )
+
+    puts "\n\n\nResponse #{response.code} #{response.message}: #{response.body}"
+    puts "Data sent: #{encoded_data}"
+    puts "Headers sent: #{headers}\n\n\n\n"
   end
 end
