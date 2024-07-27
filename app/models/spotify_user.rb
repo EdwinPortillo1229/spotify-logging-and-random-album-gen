fcc1bd4fba322c57fe7e10718300944d7435fd0d
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-07-27 13:49:57 -0400

    wipperoni

--- app/models/spotify_user.rb
+++ app/models/spotify_user.rb
@@ -6,7 +6,7 @@ class SpotifyUser < ActiveRecord::Base
   SPOTIFY_URL = "https://accounts.spotify.com/authorize?"
   CLIENT_ID = "a2cb6f3e5f7340c3a1ed328590fab4b2"
   CLIENT_SECRET = "05c095d48e654dbeb5ce16a92ac8fcfb"
-  REDIRECT_URI_LINK = "http://127.0.0.1:3000/link_spotify"
+  REDIRECT_URI_LINK = "https://spotify-random-liked-albums-20d00a4c63a7.herokuapp.com/link_spotify"
 
   def self.generate_state
     SecureRandom.hex(16)
