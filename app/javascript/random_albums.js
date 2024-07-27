82e9a5afc8aaec5517123b4b2a16d2a675d83e5b
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-07-27 00:13:06 -0400

    wipperoni

--- app/javascript/random_albums.js
+++ app/javascript/random_albums.js
@@ -1,14 +1,44 @@
-const createAlbumUI = (data) => {
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  const randomizer = document.querySelector('#get-random-albums-link');
+  const createAlbumUI = (data) => {
+    const randomAlbumContainer = document.querySelector("#randomized-albums-container");
 
+    let albumDivs = '';
+
+    data.forEach(album => {
+      albumDivs += `
+        <div class="album-div">
+          <div class="image-container">
+            <img src="${album.image_url}" class="album-image">
+          </div>
+          <div class="album-info-container">
+            <p class="album-title">
+              ${album.name}
+            </p>
+            <p class="album-artist">
+              ${album.artist}
+            </p>
+            <p class="album-release_date">
+              ${album.release_date}
+            </p>
+            <p class="album-total_tracks">
+              ${album.total_tracks}
+            </p>
+          </div>
+        </div>
+      `
+    });
+
+    randomAlbumContainer.innerHTML = albumDivs;
+  }
+
+  const randomizer = document.querySelector('#get-random-albums-link');
   randomizer.addEventListener('click', async () => {
+    const randomAlbumContainer = document.querySelector("#randomized-albums-container");
+    randomAlbumContainer.innerHTML = '';
     const userId =  document.querySelector("#hidden-user-id-input").dataset.userId;
     const res = await fetch( `/get_random_albums/${userId}`);
     const data = await res.json();
 
-    createAlbumUI(data);
+    createAlbumUI(data.albums);
   });
 });
